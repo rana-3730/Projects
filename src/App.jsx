40196a949d77a173fd465b98c7ad430df2beabce
raja_rana <basantirana934@gmail.com>
2025-03-11 08:26:52 +0530

    Restarting post-components project:

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
 import './App.css'
 import {useState} from "react"
+import PostComponent from "../components/Post.jsx"
 function App() {
+    const posts = [{}]
   return (
-    <>
-         <ToggleMessage></ToggleMessage>
-        <ToggleMessage></ToggleMessage>
-        <ToggleMessage></ToggleMessage>
-    </>
+      <>
+          <div style={{background: "#dfe6e9", height: "100vh",}}>
+              <button >Add post</button>
+              <div style={{display: "flex", justifyContent: "center"}}>
+                  <div>
+                      <PostComponent
+                          name={"harkirat"}
+                          subtitle={"10000 followers"}
+                          time={"2m ago"}
+                          image={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+                          description={"What to know how to win big? Check out how the"}
+                      ></PostComponent>
+                  </div>
+              </div>
+          </div>
+
+
+      </>
   )
 }
-
-
-
-
-   function ToggleMessage(){
-        const [isVisible , setIsVisible] = useState(false); // defining a new variable
-        // When the value of a state variable changes,
-       // the component that uses the state variables re-renders
-
-       function toggle(){
-           setIsVisible(prev => !prev)
-       }
-       return <div>
-           <button onClick={toggle}>
-               Toggle Message
-           </button>
-           {isVisible && <h3>hello there</h3> }
-
-             </div>
-
-
-    }
-
 
 
 export default App
