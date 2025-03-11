866f445c95b9294f090731236f24c6a7400bf8db
raja_rana <basantirana934@gmail.com>
2025-03-11 09:00:29 +0530

    completed Post-components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import './App.css'
 import {useState} from "react"
 import PostComponent from "../components/Post.jsx"
 function App() {
-    const posts = [{
-        name:"harkirat",
-        subtitle:"10000 followers",
-        time:"2m ago",
-        image:"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-        description:"What to know how to win big? Check out how the"
-    }]
+     const [posts , setPosts] = useState([]);
+
     // postComponents => is an array of PostComponents
     const postComponents = posts.map(post => <PostComponent
         name={post.name}
@@ -21,11 +16,19 @@ function App() {
 
 
     function addPost() {
+        setPosts([...posts, {
+            name: "harkirat",
+            subtitle: "10000 followers",
+            time: "2m ago",
+            image: "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+            description: "What to know how to win big? Check out how these folks won $6000 in bounties."
+
+        }])
 
     }
   return (
       <>
-          <div style={{background: "#dfe6e9", height: "100vh",}}>
+          <div style={{background: "#dfe6e9", height: "100%",}}>
               <button onClick={addPost} >Add post</button>
               <div style={{display: "flex", justifyContent: "center"}}>
                   <div>
