69cf938e4fb030df4f3837d32824e7553e547f7b
raja_rana <basantirana934@gmail.com>
2025-03-12 05:57:14 +0530

    using of dependendies array for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,42 @@ import './App.css'
 import {useState , useEffect } from "react"
 
 function App() {
-    const [count , setCount ] = useState(0) ;
-    // useEffect => lets you manage side-effects
-    // side-effects => other than rendering the components
-    // starting a clock , fetch data from an api, websockets are  side-effect
-    // Problems => if you directly try to introduce side effects directly in the rendering
-    // logic of a component , React would run that code every time the components renders.
-    // This lead to => unnecessary effects , inconsistent behaviour , performance issues
-
-    // runs on mount
+    const [currentTab , setCurrentTab] = useState("1")
+    const [tabData , setTabData ] = useState({})
+    const [loading , setLoading ] = useState(true);
     useEffect(() => {
-        console.log("above setInterval");
-        setInterval(() => {
-            increaseCount()
-            }
-        , 1000)
-    }, []);
-    // setInterval(increaseCount,1000) => runs on every render
+        // jsonplaceholder => free fake api
+        // send a backend request
+        setLoading(true)
+        console.log("request is send to backend to get data for tab  " + currentTab)
+        fetch('https://jsonplaceholder.typicode.com/todos/' + currentTab)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                setTabData(json)
+                setLoading(false)
 
-    function increaseCount() {
-        setCount(((currValue) => {
-            console.log(`Increase count called ${currValue}`)
-            return currValue + 1 ;
-        }  ));
+            })
+    }, [currentTab]);
 
-        // setCount can either take a final value for count or
-        // can take a function which uses current value to get new valuec
+    function changeTab(value){
+        setCurrentTab(value);
     }
+
+
 
   return (
       <>
           <div>
-              <div style={{backgroundColor:"red" , width:20 , height:20 , borderRadius:20, marginLeft:5 , display:"flex", justifyContent:"center"}}>
-                  {count}</div>
-              <img src={"https://as1.ftcdn.net/v2/jpg/02/04/42/24/1000_F_204422460_EPBVMqQVwYI7vjMoHycZ0C1ZLZcDAVhE.jpg"}
-              style={{height:30 , width:30}} onClick={increaseCount
-              }/>
+              <button onClick={() => changeTab("1")} style={{color: currentTab === "1" ? "red" : "black"}}>Todo #1</button>
+              <button onClick={()=> changeTab("2")} style={{color: currentTab === "2" ? "red" : "black"}}>Todo #2</button>
+              <button onClick={()=> changeTab("3")} style={{color: currentTab === "3" ? "red" : "black"}}>Todo #3</button>
+              <button onClick={() => changeTab("4")} style={{color: currentTab === "4" ? "red" : "black"}}>Todo #4 </button>
+              <br />
+              <div>
+                  { loading ? "loading..." : tabData.title}
+              </div>
+
           </div>
 
       </>
