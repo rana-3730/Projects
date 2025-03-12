799362d0af0915f3428f02cf0411c1db91cbd7fa
raja_rana <basantirana934@gmail.com>
2025-03-12 08:02:47 +0530

    Introducing ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,61 @@
 import './App.css'
+import React from 'react';
 import {useState , useEffect } from "react"
 
 function App() {
 
+    // These events
+
     return (
         <>
-            <Todo key={1} title={"Go to gym"} done={false}></Todo>
-            <Todo key={2} title={"Eat food"} done={false}></Todo>
+            <ErrorBoundary>
+                <Card1></Card1>
+            </ErrorBoundary>
+
+            <Card2></Card2>
+
 
         </>
     )
 
 }
-
-function Todo({title ,done }){
-    return <div>
-        {title} - {done?"Done" : "Not done"}
-
+function Card1(){
+    throw new Error("App is no working ")
+    return <div style={{background: "red" , borderRadius:10 ,padding:10 , margin: 5}}>
+        Hi there
+    </div>
+}
+function Card2(){
+    return <div style={{background: "red" , borderRadius:10 ,padding:10, margin:5}}>
+        Hi there
     </div>
 }
 
 
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
 
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.error("Error caught:", error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <div style={{background: "red", borderRadius: 10, padding: 10, margin: 5}}>
+                Something went wrong
+            </div>
+        }
+
+        return this.props.children;
+    }
+}
 
 
 export default App
