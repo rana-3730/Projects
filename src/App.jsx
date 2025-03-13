19d3aa9b77e79ee91b4b4d3db177be62ce794159
raja_rana <basantirana934@gmail.com>
2025-03-13 05:35:20 +0530

    Use of Link and useNavigate hook in single page app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import './App.css'
 import React from 'react';
-import {BrowserRouter, Routes , Route } from "react-router-dom"
+import {BrowserRouter, Routes , Route, Link, useNavigate } from "react-router-dom"
 import {useState , useEffect } from "react"
 
 function App() {
-
-
+    // single page app => re-fetching some parts instead of full page fetching
     return (
         <>
             hello there
+
             <BrowserRouter>
+                <Link to={"/"}>Allen</Link> |
+                <Link to={"/neet/online"}>Neet</Link> |
+                <Link to={"/jee/online"}>Jee</Link>
                 <Routes>
                     <Route path="/neet/online" element={<Neet />}></Route>
                     <Route path="/jee/online" element={<Jee></Jee>}></Route>
                     <Route path="/" element={<Home></Home>}></Route>
+                    <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
                 </Routes>
             </BrowserRouter>
         </>
     )
 
+}
+function ErrorPage(){
+    return <div>
+        Sorry page not found
+    </div>
 }
 function Neet(){
     return <div>
@@ -26,8 +35,15 @@ function Neet(){
     </div>
 }
 function Jee() {
+    const navigate = useNavigate();
+
+    function redirectUser(){
+        navigate("/")
+    }
     return <div>
         Hello Jee aspirants
+        <br />
+        <button onClick={redirectUser}>Home</button>
     </div>
 }
 
@@ -36,5 +52,6 @@ function Home(){
         Hello there aspirants
     </div>
 }
+
 
 export default App ;
