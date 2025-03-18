671c1c61d9fdeae5a1e605e63ae781e33c17f7ac
raja_rana <basantirana934@gmail.com>
2025-03-18 18:27:16 +0530

    Routing Revision

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,78 @@
 import './App.css'
 import React from 'react';
-import {BrowserRouter, Routes , Route, Link, useNavigate , Outlet } from "react-router-dom"
-import {useState , useEffect } from "react"
+import { BrowserRouter , Routes , Route , Link , useNavigate , Outlet} from "react-router-dom";
+// HashRouter => browser extension
 
 function App() {
-    // single page app => re-fetching some parts instead of full page fetching
+    // single page application => some part of page will change
+    // great user experience => avoid full page
+    // Link => user click
+    // useNavigate => automatic navigate
+    // Layouts => Next.js
+    // custom array like routes = [{ path : "" , element: <element>} ]
+    // then map to them into <Route> </Route> components
+    // useRef =>
+
     return (
         <>
+
+            {/* the naive way is to wrap above all inside anchor tag */}
             <BrowserRouter>
+                <Link to="/home" >Allen Neet</Link>  | <Link to="/neet/online-coaching/class11">Class 11 </Link>|
+                <Link to="/neet/online-coaching/class12">Class 12</Link>
 
-
-
-               <Routes>
-                   <Route path="/" element={<Layout></Layout>}>
-
-                        <Route path="/neet/online" element={<Neet />}></Route>
-                        <Route path="/jee/online" element={<Jee></Jee>}></Route>
-                        <Route path="/home" element={<Home></Home>}></Route>
-                        <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
+                <Routes>
+                   <Route path="/" element={<Layout />}>
+                       { /* What is path variable *!/ {}*/}
+                       {/* It restricts the random url   */}
+                       <Route path="/neet/online-coaching/class11" element={<Class11Program /> }></Route>
+                       <Route path="/neet/online-coaching/class12" element={<Class12Program /> }></Route>
+                       <Route path="/Home" element={<Landing />}></Route>
+                       <Route path="*" element={<ErrorPage/>} />
+                       {/* manage random routes */}
                    </Route>
-
                 </Routes>
             </BrowserRouter>
         </>
     )
 
 }
+function Layout(){
+return <div>
 
-function Layout() {
-    return <div style={{height :"100vh" ,  backgroundColor: "red "}}>
-        <Header></Header>
+   <Outlet />
+    footer
+</div>
+}
+function Landing(){
+    return <div>
+        Hello there
+    </div>
 
-        <div style={{height:"90vh" , background:"pink"}}>
-            <Outlet />
-        </div>
-        Footer | Contact Us
+}
+
+
+function Class11Program(){
+    return <div>
+        Neet Program for 11 class
     </div>
 }
-function Header(){
+
+function Class12Program(){
+    const navigate = useNavigate();
+    function redirectUser(){
+        navigate("/")
+    }
+
     return <div>
-        <Link to={"/home"}>Allen</Link> |
-        <Link to={"/neet/online"}>Neet</Link> |
-        <Link to={"/jee/online"}>Jee</Link>
+        Neet Program from 12 class
+        <button onClick={redirectUser}>Go back to landing page </button>
     </div>
+
 }
 function ErrorPage(){
     return <div>
-        Sorry page not found
+        Page not found
     </div>
 }
-function Neet(){
-    return <div>
-        Hello Neet aspirants
-    </div>
-}
-function Jee() {
-    const navigate = useNavigate();
-
-    function redirectUser(){
-        navigate("/home")
-    }
-    return <div>
-        Hello Jee aspirants
-        <br />
-        <button onClick={redirectUser}>Home</button>
-    </div>
-}
-
-function Home(){
-    return <div>
-        Hello there aspirants
-    </div>
-}
-
-
 export default App ;
