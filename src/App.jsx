3461b5e08239ef3040ca1f63f6e5964c4b46592d
raja_rana <basantirana934@gmail.com>
2025-03-18 18:46:01 +0530

    Introduction of useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,29 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter , Routes , Route , Link , useNavigate , Outlet} from "react-router-dom";
-// HashRouter => browser extension
+import {useRef} from 'react';
+
+
 
 function App() {
-    // single page application => some part of page will change
-    // great user experience => avoid full page
-    // Link => user click
-    // useNavigate => automatic navigate
-    // Layouts => Next.js
-    // custom array like routes = [{ path : "" , element: <element>} ]
-    // then map to them into <Route> </Route> components
-    // useRef =>
-
-    return (
-        <>
-
-            {/* the naive way is to wrap above all inside anchor tag */}
-            <BrowserRouter>
-                <Link to="/home" >Allen Neet</Link>  | <Link to="/neet/online-coaching/class11">Class 11 </Link>|
-                <Link to="/neet/online-coaching/class12">Class 12</Link>
-
-                <Routes>
-                   <Route path="/" element={<Layout />}>
-                       { /* What is path variable *!/ {}*/}
-                       {/* It restricts the random url   */}
-                       <Route path="/neet/online-coaching/class11" element={<Class11Program /> }></Route>
-                       <Route path="/neet/online-coaching/class12" element={<Class12Program /> }></Route>
-                       <Route path="/Home" element={<Landing />}></Route>
-                       <Route path="*" element={<ErrorPage/>} />
-                       {/* manage random routes */}
-                   </Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
-
-}
-function Layout(){
-return <div>
-
-   <Outlet />
-    footer
-</div>
-}
-function Landing(){
-    return <div>
-        Hello there
-    </div>
-
-}
+    const inputRef = useRef();
+    // useRef => like id attribute in css it provide a ref attribute
+    // we can give a name to ref
+    // useRef => reference to a value, such that when u change the value ,
+    // the component DOES NOT RE-RENDER
 
 
-function Class11Program(){
-    return <div>
-        Neet Program for 11 class
-    </div>
-}
+    function focusOnInput(){
+        inputRef.current.focus();
 
-function Class12Program(){
-    const navigate = useNavigate();
-    function redirectUser(){
-        navigate("/")
+
     }
-
     return <div>
-        Neet Program from 12 class
-        <button onClick={redirectUser}>Go back to landing page </button>
+        Sign up
+        <input ref={inputRef} type={"text"}/>
+        <input type={"text"}></input>
+        <button onClick={focusOnInput}>Submit</button>
     </div>
 
 }
-function ErrorPage(){
-    return <div>
-        Page not found
-    </div>
-}
+
 export default App ;
