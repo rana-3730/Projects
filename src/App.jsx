2e4da531f1d9a7c14cd81088ea28ac99e4077c78
raja_rana <basantirana934@gmail.com>
2025-03-26 17:18:44 +0530

    Props drilling and Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import './App.css'
 import React from 'react';
-import {useState , useEffect , useRef } from 'react';
+import {useState , createContext, useContext} from 'react';
 
-
+const BulbContext = createContext();
 
 function App() {
-  const [currentCount , setCurrentCount] = useState(1);
-  // const [timer , setTimer] = useState(0);
-  const timer = useRef()
+  // Rolling up the state, un optimal re-renders => passing state using props
+  // multiple components need access to the same state
+  // Prop drilling => drilling props through all components
+  // Context API => Application Programming Interface => manage state more effectively and solve props drilling
 
-  // This will run only one time for one click on start button
-  function startClock() {
-      console.log("clock started")
-      let value = setInterval(()=>{
-          setCurrentCount(currentCount => currentCount + 1 )
-      } , 1000)
-      // setTimer(value)
-      timer.current = value
-  }
-  function stopClock() {
-      clearInterval(timer.current);
-  }
+  // Context: React.createContext() => serve as a data storage
+  const [bulbOn , setBulbOn] = useState(false) ;
 
-    return <div>
-        {currentCount}
-        <br />
-        <button onClick={startClock}>Start</button>
-        <button onClick={stopClock}>Stop</button>
+  return <>
+    <BulbContext.Provider value={{
+      bulbOn,
+      setBulbOn
+    }}>
+      <Light></Light>
+    </BulbContext.Provider>
 
-    </div>
+  </>
+}
 
+
+function Light() {
+  return <div>
+    <LightBulb />
+    <LightSwitch />
+  </div>
+}
+
+function LightBulb() {
+  const {bulbOn} = useContext(BulbContext);
+  return <div>
+
+  </div>
+}
+function LightSwitch () {
+  return <div>
+    <button>Bulb on </button>
+
+  </div>
 }
 
 export default App ;
